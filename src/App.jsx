beb97e60c23049a883949477cfcf1e688b588e38
Sindhuja2535 <sindhuja2516@gamil.com>
2025-06-01 18:41:55 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,39 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
 import Product from "./components/Product";
 import Cart from "./components/Cart";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Home from "./components/Home";
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
-    <div>
-      <BrowserRouter>
-        <header>
-          <h1>My Online Shop</h1>
-          <Link to="/">Home</Link> -
-          <Link to="/cart">Cart</Link> -
-          <Link to="/login">Login</Link>
-          <hr />
-        </header>
+    <BrowserRouter>
+      <header>
+        <h1>Online Shopping Section</h1>
+        <nav>
+          <Link to="/">Home</Link> - <Link to="/cart">Cart</Link> - <Link to="/login">Login</Link>
+        </nav>
+        <hr />
+      </header>
 
-        <main>
-          <Routes>
-            <Route path="/" element={<Product />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/home" element={<Home />} />
-          </Routes>
-        </main>
+      <Routes>
+        <Route path="/" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
+        <Route path="/register" element={<Register setUser={setUser} />} />
+        <Route path="/home" element={<Home user={user} />} />
+      </Routes>
 
-        <footer>
-          <hr />
-          &copy; 2005. All rights Reserved.
-        </footer>
-      </BrowserRouter>
-    </div>
+      <footer>
+        <hr />
+        &copy; 2025. All rights Reserved.
+      </footer>
+    </BrowserRouter>
   );
 }
+
 export default App;
