6e49b2361aca8d12b33549238f87614a35f9ba27
Sindhuja2535 <sindhuja2516@gamil.com>
2025-06-02 15:19:57 +0530

    ,

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useState } from "react";
 import { useContext } from "react";
 import { AppContext } from "../App";
@@ -5,6 +6,7 @@ import axios from "axios";
 export default function Product() {
   const { user } = useContext(AppContext);
   const [products, setProducts] = useState([]);
+  
   const fetchProducts = async () => {
     const res = await axios.get("http://localhost:8080/products");
     setProducts(res.data);
